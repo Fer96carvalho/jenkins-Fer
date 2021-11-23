d7e2edef9db9bda36aeb75a10df778fb2a7fb9fa
fer96carvalho <f.luiz1996@gmail.com>
2021-11-23 09:10:58 -0400

    Projeto jenkins

---
+++ src/main/java/dev/fer/jenkinsFer/JenkinsFerApplication.java
@@ -0,0 +1,13 @@
+package dev.fer.jenkinsFer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsFerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fer/jenkinsFer/JenkinsFerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fer.jenkinsFer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsFerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
